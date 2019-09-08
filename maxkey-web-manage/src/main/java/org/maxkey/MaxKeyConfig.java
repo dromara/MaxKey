3c9c422fc7aecf3cd4637303e0d438785d492a54
shimingxy <shimingxy@163.com>
2019-09-08 15:45:45 +0800

    springboot init
    springboot init

--- maxkey-web-manage/src/main/java/org/maxkey/MaxKeyConfig.java
+++ maxkey-web-manage/src/main/java/org/maxkey/MaxKeyConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource(locations={"classpath:spring/maxkey_mgt.xml"})
+@ImportResource(locations={"classpath:spring/maxkey-mgt.xml"})
 public class MaxKeyConfig {
 
 }
