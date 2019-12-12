a16424821829d19ebd95c5cea6108f07800550e7
shimingxy <shimingxy@163.com>
2019-12-12 22:21:56 +0800

    build r

--- buildrelease.bat
+++ buildrelease.bat
@@ -3,9 +3,6 @@ call setenv.bat
 set START_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
 echo start time %START_TIME%
 
-call %JAVA_HOME%/bin/java -version
-call %GRADLE_HOME%/bin/gradle -version
-
 call %GRADLE_HOME%/bin/gradle checkenv
 
 call %GRADLE_HOME%/bin/gradle
--- setenv.bat
+++ setenv.bat
@@ -2,3 +2,6 @@ echo off
 echo set env
 set JAVA_HOME=D:\JavaIDE\jdk1.8.0_91
 set GRADLE_HOME=D:\JavaIDE\gradle-5.4.1
+
+call %JAVA_HOME%/bin/java -version
+call %GRADLE_HOME%/bin/gradle -version
