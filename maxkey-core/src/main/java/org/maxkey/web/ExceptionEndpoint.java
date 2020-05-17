3b867347fa0364837e270f47d3ab99a028acd0b3
shimingxy <shimingxy@163.com>
2020-05-17 10:57:37 +0800

    ExceptionEndpoint fix
    ExceptionEndpoint fix

--- maxkey-core/src/main/java/org/maxkey/web/ExceptionEndpoint.java
+++ maxkey-core/src/main/java/org/maxkey/web/ExceptionEndpoint.java
@@ -1,5 +1,7 @@
 package org.maxkey.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,33 +9,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Index
+ * Exception.
+ * 
  * @author Crystal.Sea
  *
  */
 @Controller
 public class ExceptionEndpoint {
-	private static Logger _logger = LoggerFactory.getLogger(ExceptionEndpoint.class);
-	
-	@RequestMapping(value={"/exception/error/400"})
-	public ModelAndView error400() {
-		_logger.debug("exception/400.");
-		return  new ModelAndView("exception/400");
-	}
-	
-	@RequestMapping(value={"/exception/error/404"})
-	public ModelAndView error404() {
-		_logger.debug("exception/404.");
-		return  new ModelAndView("exception/404");
-	}
-	@RequestMapping(value={"/exception/error/500"})
-	public ModelAndView error500() {
-		_logger.debug("exception/500.");
-		return  new ModelAndView("exception/500");
-	}
-	@RequestMapping(value={"/exception/accessdeny"})
-	public ModelAndView accessdeny() {
-		_logger.debug("exception/accessdeny.");
-		return  new ModelAndView("exception/accessdeny");
-	}
+    private static Logger _logger = LoggerFactory.getLogger(ExceptionEndpoint.class);
+
+    @RequestMapping(value = { "/exception/error/400" })
+    public ModelAndView error400(
+            HttpServletRequest request, HttpServletResponse response) {
+        _logger.debug("exception/400 ");
+        return new ModelAndView("exception/400");
+    }
+
+    /**
+     * //查看浏览器中的报错信息.
+     * @param request HttpServletRequest
+     * @param response HttpServletResponse
+     * @return
+     */
+    @RequestMapping(value = { "/exception/error/404" })
+    public ModelAndView error404(
+            HttpServletRequest request, HttpServletResponse response) {
+        
+        _logger.debug("exception/404 ");
+        return new ModelAndView("exception/404");
+    }
+
+    @RequestMapping(value = { "/exception/error/500" })
+    public ModelAndView error500(HttpServletRequest request, HttpServletResponse response) {
+        _logger.debug("exception/500 ");
+        return new ModelAndView("exception/500");
+    }
+
+    @RequestMapping(value = { "/exception/accessdeny" })
+    public ModelAndView accessdeny(HttpServletRequest request, HttpServletResponse response) {
+        _logger.debug("exception/accessdeny ");
+        return new ModelAndView("exception/accessdeny");
+    }
 }
