439c801382ae4ad846fc569fb6b27798ccceff11
shimingxy <shimingxy@163.com>
2020-06-13 10:50:36 +0800

    Update MemberRef.java

--- maxkey-identitys/maxkey-identity-scim/src/main/java/org/maxkey/identity/scim/resources/MemberRef.java
+++ maxkey-identitys/maxkey-identity-scim/src/main/java/org/maxkey/identity/scim/resources/MemberRef.java
@@ -2,4 +2,9 @@ package org.maxkey.identity.scim.resources;
 
 public class MemberRef extends MultiValuedAttribute{
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -6018893424394625889L;
+
 }
