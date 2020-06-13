b6c8d214935421d7acd0f0b172178dd68c52fc6a
shimingxy <shimingxy@163.com>
2020-06-13 10:43:26 +0800

    Update EnterpriseUser.java

--- maxkey-identitys/maxkey-identity-scim/src/main/java/org/maxkey/identity/scim/resources/EnterpriseUser.java
+++ maxkey-identitys/maxkey-identity-scim/src/main/java/org/maxkey/identity/scim/resources/EnterpriseUser.java
@@ -6,6 +6,8 @@ public class EnterpriseUser extends User {
      */
     private static final long serialVersionUID = 3212312511630459427L;
     
+    public static final String SCHEMA = "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User";
+    
     Enterprise enterprise;
     
     public EnterpriseUser() {
