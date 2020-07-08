16a46bdd728a7d7b56010e5ac85e39b9efc58cfb
MaxKey <shimingxy@163.com>
2020-07-08 23:52:41 +0800

    Update gradleSetEnv.bat

--- gradleSetEnv.bat
+++ gradleSetEnv.bat
@@ -1,7 +1,7 @@
 echo off
 echo set env
 set JAVA_HOME=D:\JavaIDE\jdk1.8.0_91
-set GRADLE_HOME=D:\JavaIDE\gradle-5.4.1
+set GRADLE_HOME=D:\JavaIDE\gradle-6.5.1
 
 call %JAVA_HOME%/bin/java -version
 call %GRADLE_HOME%/bin/gradle -version
