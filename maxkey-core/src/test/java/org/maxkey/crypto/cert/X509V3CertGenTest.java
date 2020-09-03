99e778f9ae7b2d8f947d8e224068ae26dd6401fc
Crystal.Sea <shimingxy@163.com>
2020-09-03 21:50:56 +0800

    run @Test

--- maxkey-core/src/test/java/org/maxkey/crypto/cert/X509V3CertGenTest.java
+++ maxkey-core/src/test/java/org/maxkey/crypto/cert/X509V3CertGenTest.java
@@ -17,6 +17,7 @@
 
 package org.maxkey.crypto.cert;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.security.KeyPair;
 import java.security.Security;
@@ -33,15 +34,20 @@ public class X509V3CertGenTest {
 	public void generateV3() throws Exception {
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		KeyPair keyPair =X509V3CertGen.genRSAKeyPair();
-		String issuer="CN=connsec.com,O=connsec,L=SH,ST=SH,C=CN";
+		String issuer="CN=maxkey.top,O=maxkey,L=SH,ST=SH,C=CN";
 		Date startDate=DateTime.now().toDate();
 		Date endDate=DateTime.now().plusMonths(10).toDate();
 		System.out.println("Private : "+ keyPair.getPrivate().toString());
 	  
 		System.out.println("Public : "+ keyPair.getPublic().toString());
 		X509Certificate cert = X509V3CertGen.genV3Certificate(issuer,issuer,startDate,endDate,keyPair);
-	   
-		FileOutputStream out = new FileOutputStream("c:\\Cert345.cer");
+		String certFileString = "D:\\MaxKey\\Workspaces\\maxkey\\Cert345.cer";
+	    File certFile =new File(certFileString);
+	    if(certFile.exists()) {
+	        certFile.deleteOnExit();
+	    }
+	    
+		FileOutputStream out = new FileOutputStream(certFileString);
 		out.write(cert.getEncoded());
 		out.close();
 	
