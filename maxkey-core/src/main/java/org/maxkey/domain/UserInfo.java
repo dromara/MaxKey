a555a251b1cc3c20db5784fa81f94b16f57c6ed1
Crystal.Sea <shimingxy@163.com>
2020-11-10 22:47:11 +0800

    Revert "Merge pull request #35 from FranisiL/master_dev_lyp"
    This reverts commit 60a40708b8c799c3c1bf95d4a1055f2406f37210, reversing
    changes made to d96247e741cb82a285edda050f516ee7423baf54.

--- maxkey-core/src/main/java/org/maxkey/domain/UserInfo.java
+++ maxkey-core/src/main/java/org/maxkey/domain/UserInfo.java
@@ -1,19 +1,19 @@
 /*
  * Copyright [2020] [MaxKey of copyright http://www.maxkey.top]
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 
 package org.maxkey.domain;
 
@@ -32,7 +32,7 @@ import org.springframework.web.multipart.MultipartFile;
 /**
  * .
  * @author Crystal.Sea
- *
+ * 
  */
 @Table(name = "MXK_USERINFO")
 public class UserInfo extends JpaBaseDomain {
@@ -329,7 +329,7 @@ public class UserInfo extends JpaBaseDomain {
     }
 
     /**
-     *
+     * 
      */
     public UserInfo() {
         super();
@@ -469,7 +469,7 @@ public class UserInfo extends JpaBaseDomain {
         this.nickName = nickName;
     }
 
-
+   
 
     public String getNameZhSpell() {
         return nameZhSpell;
@@ -1147,6 +1147,7 @@ public class UserInfo extends JpaBaseDomain {
         this.description = description;
     }
 
+ 
     /**
      * @return the createdBy
      */
