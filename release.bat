6e8356894591ea8a388586a72f1df7e16ed12492
Crystal.Sea <shimingxy@163.com>
2020-11-18 23:48:22 +0800

    buildRelease

--- release.bat
+++ release.bat
@@ -2,9 +2,11 @@ call setEnvVars.bat
 
 set START_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
 echo start time %START_TIME%
-
 call %JAVA_HOME%/bin/java -version
+
 call %GRADLE_HOME%/bin/gradle -version
+
+call %GRADLE_HOME%/bin/gradle -q projects
 
 echo start clean . . .
 
@@ -12,14 +14,9 @@ call %GRADLE_HOME%/bin/gradle clean
 
 echo start clean complete . 
 
-call %GRADLE_HOME%/bin/gradle checkenv
-
-call %GRADLE_HOME%/bin/gradle
-
-call %GRADLE_HOME%/bin/gradle war
-
 call %GRADLE_HOME%/bin/gradle buildRelease
 
+call %GRADLE_HOME%/bin/gradle copyDepJars
 
 cd build
 rd /q /s libs
