22b5632b32484eb7ef2c0a1779aaffebc21265fc
MaxKey <shimingxy@qq.com>
2021-02-08 21:12:12 +0800

    Update Copyright.java

--- maxkey-core/src/test/java/org/maxkey/Copyright.java
+++ maxkey-core/src/test/java/org/maxkey/Copyright.java
@@ -26,12 +26,12 @@ import java.io.OutputStreamWriter;
  
 /**
  * 给java文件批量添加License信息.
- * @author tanghc
+ * @author MaxKey Copyright Adder
  *
  */
 public class Copyright {   
     // 存放java文件的文件夹,必须是文件夹
-    private static String srcFolder = "D:\\MaxKey\\Workspaces\\maxkey\\MaxKey";
+    private static String srcFolder = "C:\\IDES\\shimi\\eclipse-workspace\\MaxKey";
     //已添加标识
     private static String copyRightText = "http://www.apache.org/licenses/LICENSE-2.0";
     //扫描目录
