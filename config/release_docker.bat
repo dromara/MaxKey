2b759df576e179cea77571f98bc9a0e868a60010
MaxKey <shimingxy@qq.com>
2021-03-07 22:53:16 +0800

    release docker bootjar standard
    release docker bootjar standard

---
+++ config/release_docker.bat
@@ -0,0 +1,32 @@
+call setEnvVars.bat
+
+set START_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
+echo start time %START_TIME%
+call %JAVA_HOME%/bin/java -version
+
+call %GRADLE_HOME%/bin/gradle -version
+
+call %GRADLE_HOME%/bin/gradle -q projects
+
+echo start clean . . .
+
+call %GRADLE_HOME%/bin/gradle clean
+
+echo start clean complete . 
+
+call %GRADLE_HOME%/bin/gradle buildRelease
+
+call %GRADLE_HOME%/bin/gradle jib
+
+cd build
+rd /q /s libs
+
+rd /q /s tmp
+
+cd ..
+
+set END_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
+
+echo Build Release start at %START_TIME%  complete at %END_TIME%.
+
+pause
---
+++ release_cnf_jar.bat
@@ -0,0 +1,11 @@
+call setEnvVars.bat
+
+set START_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
+echo start time %START_TIME%
+call %JAVA_HOME%/bin/java -version
+
+call %GRADLE_HOME%/bin/gradle -version
+
+call %GRADLE_HOME%/bin/gradle -b build_cnf_jar.gradle
+
+pause
