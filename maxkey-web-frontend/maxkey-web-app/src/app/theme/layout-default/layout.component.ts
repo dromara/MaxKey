7047d5f4e120074f7e4ad89d079fda07654000ce
MaxKey <shimingxy@qq.com>
2022-05-26 15:50:54 +0800

    menu  align left

--- maxkey-web-frontend/maxkey-web-app/src/app/theme/layout-default/layout.component.ts
+++ maxkey-web-frontend/maxkey-web-app/src/app/theme/layout-default/layout.component.ts
@@ -1,19 +1,18 @@
 /*
  * Copyright [2022] [MaxKey of copyright http://www.maxkey.top]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
 
 import { DOCUMENT } from '@angular/common';
 import { Component, ContentChildren, ElementRef, Inject, Input, OnDestroy, OnInit, QueryList, Renderer2, TemplateRef } from '@angular/core';
@@ -38,7 +37,7 @@ import { LayoutDefaultOptions } from './types';
     <div id="mainMenu" nzTheme="dark" style="background: #001529;" class="alain-default__fixed">
       <!--<ng-container *ngTemplateOutlet="asideUser"></ng-container>-->
       <ng-container *ngTemplateOutlet="nav"></ng-container>
-      <layout-default-nav *ngIf="!nav" class="d-block" style="margin-top: 64px; width:80%"></layout-default-nav>
+      <layout-default-nav *ngIf="!nav" class="d-block" style="margin-top: 64px; "></layout-default-nav>
     </div>
 
     <section class="alain-default__content">
