a129899acc0e5cea497918ca423ca2811b54db48
MaxKey <shimingxy@qq.com>
2022-07-07 15:37:21 +0800

    oauth2-approve

--- maxkey-web-frontend/maxkey-web-app/src/app/routes/authz/oauth2-approve/oauth2-approve.component.ts
+++ maxkey-web-frontend/maxkey-web-app/src/app/routes/authz/oauth2-approve/oauth2-approve.component.ts
@@ -1,19 +1,18 @@
 /*
  * Copyright [2022] [MaxKey of copyright http://www.maxkey.top]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
 
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, Inject, OnDestroy, Optional } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -40,7 +39,9 @@ export class Oauth2ApproveComponent implements OnInit {
     };
   } = {
       submitting: false,
-      model: {}
+      model: {
+        approval_prompt: 'force'
+      }
     };
   redirect_uri: string = '';
   formGroup: FormGroup = new FormGroup({});
@@ -76,7 +77,9 @@ export class Oauth2ApproveComponent implements OnInit {
     //if (this.formGroup.valid) {
     this.oauth2ApproveService.approval(this.form.model).subscribe(res => {
       if (res.code == 0) {
-        this.msg.success(`提交成功`);
+        if (this.form.model.approval_prompt == 'force') {
+          this.msg.success(`提交成功`);
+        }
         window.location.href = res.data;
       } else {
         this.msg.success(`提交失败`);
