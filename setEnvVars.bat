94b7d3c66275db9c1663ece03298a0cfc81f8d64
MaxKey <shimingxy@qq.com>
2022-10-09 19:14:20 +0800

    setEnvVars gradle-7.5.1

--- setEnvVars.bat
+++ setEnvVars.bat
@@ -1,7 +1,7 @@
 echo off
 echo set env
 set JAVA_HOME=D:\IDE\jdk-8.0.312.7
-set GRADLE_HOME=C:\IDE\gradle-7.2
+set GRADLE_HOME=C:\IDE\gradle-7.5.1
 
 call %JAVA_HOME%/bin/java -version
 call %GRADLE_HOME%/bin/gradle -version
