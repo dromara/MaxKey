e4b9ed463b8bbf62fef2ce748b7b24a37df50276
MaxKey <shimingxy@qq.com>
2022-10-22 16:26:16 +0800

    SingleLogout trace

--- maxkey-protocols/maxkey-protocol-authorize/src/main/java/org/maxkey/authz/singlelogout/SingleLogout.java
+++ maxkey-protocols/maxkey-protocol-authorize/src/main/java/org/maxkey/authz/singlelogout/SingleLogout.java
@@ -31,7 +31,7 @@ public abstract class SingleLogout {
     public abstract void sendRequest(Authentication authentication,Apps logoutApp) ;
     
     public void postMessage(String url,Map<String, Object> paramMap) {
-    	_logger.trace("post message to url " + url);
+    	_logger.debug("post message to url {}" , url);
     	(new HttpRequestAdapter()).post(url , paramMap);
     }
 }
