91514cce7ef21195531abf0b453ed82207b047cf
MaxKey <shimingxy@qq.com>
2022-12-14 17:02:08 +0800

    v3.5.11 SQL

--- docker/docker-mysql/docker-entrypoint-initdb.d/init.sql
+++ docker/docker-mysql/docker-entrypoint-initdb.d/init.sql
@@ -2,6 +2,6 @@ create database if not exists  `maxkey` /*!40100 DEFAULT CHARACTER SET utf8mb4 C
 
 use maxkey ;
 
-source /docker-entrypoint-initdb.d/v3.5.10.ga/maxkey_v3.5.10.GA.sql   ;
-source /docker-entrypoint-initdb.d/v3.5.10.ga/maxkey_v3.5.10.GA_data.sql   ;
+source /docker-entrypoint-initdb.d/v3.5.11.ga/maxkey_v3.5.11.GA.sql   ;
+source /docker-entrypoint-initdb.d/v3.5.11.ga/maxkey_v3.5.11.GA_data.sql   ;
  
