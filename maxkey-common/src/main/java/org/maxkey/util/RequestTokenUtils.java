ae6e7fef814a52f3d9643426ffca3e935ddd2efb
MaxKey <shimingxy@qq.com>
2023-03-21 17:28:11 +0800

    Update RequestTokenUtils.java

--- maxkey-common/src/main/java/org/maxkey/util/RequestTokenUtils.java
+++ maxkey-common/src/main/java/org/maxkey/util/RequestTokenUtils.java
@@ -18,14 +18,22 @@ import org.apache.commons.lang3.StringUtils;
 
 public class RequestTokenUtils {
 	
+	public static String TOKEN 			= "token";
+	
+	public static String ACCESS_TOKEN 	= "access_token";
 	/**
-	 * resolveAccessToken
+	 * 从请求中获取token令牌信息,优先级顺序如下  
+	 * <p>
+	 *  1) 参数 access_token <br/>
+	 *  2) header 的Authorization或者authorization <br/>
+	 *  3) 参数 token <br/>
+	 * </p>
 	 * 
 	 * @param request
 	 * @return access_token
 	 */
 	public static String resolveAccessToken(HttpServletRequest request) {
-		String access_token = request.getParameter("access_token");
+		String access_token = request.getParameter(ACCESS_TOKEN);
 		
 		if(StringUtils.isBlank(access_token)) {
 	    	//for header authorization bearer
@@ -33,7 +41,7 @@ public class RequestTokenUtils {
 	    }
 		
 		if(StringUtils.isBlank(access_token)) {
-			access_token = request.getParameter("token");
+			access_token = request.getParameter(TOKEN);
 		}
 		return access_token;
 	}
