f69d2d8b9ee8beae66bd7e94e3c2d7fd7214f43f
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-11 16:22:31 +0530

    Removed Traditional Getters and Setters and added Annotations also Code Refractored

--- maxkey-core/src/main/java/org/maxkey/entity/ChangePassword.java
+++ maxkey-core/src/main/java/org/maxkey/entity/ChangePassword.java
@@ -1,259 +1,77 @@
 /*
  * Copyright [2020] [MaxKey of copyright http://www.maxkey.top]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 
 package org.maxkey.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.mybatis.jpa.persistence.JpaBaseEntity;
 
-public class ChangePassword extends JpaBaseEntity{
+@Setter
+@Getter
+@NoArgsConstructor
+public class ChangePassword extends JpaBaseEntity {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2362608803392892403L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2362608803392892403L;
 
-	private String id;
-	private String userId;
-	private String username;
-	private String email;
-	private String mobile;
-	private String windowsAccount;
-	private String employeeNumber;
-	private String displayName;
-	private String oldPassword;
-	private String password;
-	private String confirmPassword;
-	private String decipherable;
-	private String instId;
-	private int passwordSetType;
-	private String passwordLastSetTime;
-	
-	/**
-	 * 
-	 */
-	public ChangePassword() {
+    private String id;
+    private String userId;
+    private String username;
+    private String email;
+    private String mobile;
+    private String windowsAccount;
+    private String employeeNumber;
+    private String displayName;
+    private String oldPassword;
+    private String password;
+    private String confirmPassword;
+    private String decipherable;
+    private String instId;
+    private int passwordSetType;
+    private String passwordLastSetTime;
 
-	}
-	
-	public ChangePassword(String username,String password) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	public ChangePassword(UserInfo userInfo) {
-		this.setId(userInfo.getId());
-		this.setUserId(userInfo.getId());
-		this.setUsername(userInfo.getUsername());
-		this.setWindowsAccount(userInfo.getWindowsAccount());
-		this.setMobile(userInfo.getMobile());
-		this.setEmail(userInfo.getEmail());
-		this.setEmployeeNumber(userInfo.getEmployeeNumber());
-		this.setDecipherable(userInfo.getDecipherable());
-		this.setPassword(userInfo.getPassword());
-		this.setInstId(userInfo.getInstId());
-	}
-	
-	public void clearPassword() {
-		this.password ="";
-		this.decipherable = "";
-	}
-	
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
+    public ChangePassword(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
+    public ChangePassword(UserInfo userInfo) {
+        this.setId(userInfo.getId());
+        this.setUserId(userInfo.getId());
+        this.setUsername(userInfo.getUsername());
+        this.setWindowsAccount(userInfo.getWindowsAccount());
+        this.setMobile(userInfo.getMobile());
+        this.setEmail(userInfo.getEmail());
+        this.setEmployeeNumber(userInfo.getEmployeeNumber());
+        this.setDecipherable(userInfo.getDecipherable());
+        this.setPassword(userInfo.getPassword());
+        this.setInstId(userInfo.getInstId());
+    }
 
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void clearPassword() {
+        this.password = "";
+        this.decipherable = "";
+    }
 
-
-	/**
-	 * @return the uid
-	 */
-	public String getUserId() {
-		return userId;
-	}
-
-
-	/**
-	 * @param uid the uid to set
-	 */
-	public void setUserId(String uid) {
-		this.userId = uid;
-	}
-
-
-	/**
-	 * @return the username
-	 */
-	public String getUsername() {
-		return username;
-	}
-
-
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	/**
-	 * @return the oldPassword
-	 */
-	public String getOldPassword() {
-		return oldPassword;
-	}
-
-
-	/**
-	 * @param oldPassword the oldPassword to set
-	 */
-	public void setOldPassword(String oldPassword) {
-		this.oldPassword = oldPassword;
-	}
-
-
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-
-	/**
-	 * @return the decipherable
-	 */
-	public String getDecipherable() {
-		return decipherable;
-	}
-
-
-	/**
-	 * @param decipherable the decipherable to set
-	 */
-	public void setDecipherable(String decipherable) {
-		this.decipherable = decipherable;
-	}
-
-
-	public String getMobile() {
-		return mobile;
-	}
-
-
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-
-
-	public String getDisplayName() {
-		return displayName;
-	}
-
-
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	public String getWindowsAccount() {
-		return windowsAccount;
-	}
-
-
-	public void setWindowsAccount(String windowsAccount) {
-		this.windowsAccount = windowsAccount;
-	}
-
-
-	public String getEmployeeNumber() {
-		return employeeNumber;
-	}
-
-
-	public void setEmployeeNumber(String employeeNumber) {
-		this.employeeNumber = employeeNumber;
-	}
-
-	public String getInstId() {
-		return instId;
-	}
-
-	public void setInstId(String instId) {
-		this.instId = instId;
-	}
-
-	
-	public int getPasswordSetType() {
-		return passwordSetType;
-	}
-
-
-	public void setPasswordSetType(int passwordSetType) {
-		this.passwordSetType = passwordSetType;
-	}
-
-
-	public String getPasswordLastSetTime() {
-		return passwordLastSetTime;
-	}
-
-
-	public void setPasswordLastSetTime(String passwordLastSetTime) {
-		this.passwordLastSetTime = passwordLastSetTime;
-	}
-
-
-	@Override
+    @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("ChangePassword [id=");
@@ -275,5 +93,5 @@ public class ChangePassword extends JpaBaseEntity{
         builder.append("]");
         return builder.toString();
     }
-	
+
 }
--- maxkey-core/src/main/java/org/maxkey/entity/Connectors.java
+++ maxkey-core/src/main/java/org/maxkey/entity/Connectors.java
@@ -1,12 +1,12 @@
 /*
  * Copyright [2021] [MaxKey of copyright http://www.maxkey.top]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,188 +23,64 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.mybatis.jpa.persistence.JpaBaseEntity;
 import org.maxkey.pretty.impl.JsonPretty;
 
 @Entity
 @Table(name = "MXK_CONNECTORS")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Connectors extends JpaBaseEntity implements Serializable {
 
-	private static final long serialVersionUID = 4660258495864814777L;
-	@Id
-	@Column
-	@GeneratedValue(strategy = GenerationType.AUTO, generator = "snowflakeid")
-	String id;
-	@Column
-	String connName;
-	@Column
-	String scheduler;
-	@Column
-	int justInTime;
-	@Column
-	String providerUrl;
-	@Column
-	String principal;
-	@Column
-	String credentials;
-	@Column
-	String filters;
-	@Column
-	String description;
-	@Column
-	String createdBy;
-	@Column
-	String createdDate;
-	@Column
-	String modifiedBy;
-	@Column
-	String modifiedDate;
-	@Column
-	String status;
+    private static final long serialVersionUID = 4660258495864814777L;
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "snowflakeid")
+    String id;
+    @Column
+    String connName;
+    @Column
+    String scheduler;
+    @Column
+    int justInTime;
+    @Column
+    String providerUrl;
+    @Column
+    String principal;
+    @Column
+    String credentials;
+    @Column
+    String filters;
+    @Column
+    String description;
+    @Column
+    String createdBy;
+    @Column
+    String createdDate;
+    @Column
+    String modifiedBy;
+    @Column
+    String modifiedDate;
+    @Column
+    String status;
 
-	@Column
-	private String instId;
+    @Column
+    private String instId;
 
-	private String instName;
+    private String instName;
 
-	public Connectors() {
-	}
+    public Connectors(String id) {
+        this.id = id;
+    }
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getConnName() {
-		return connName;
-	}
-
-	public void setConnName(String connName) {
-		this.connName = connName;
-	}
-
-	public int getJustInTime() {
-		return justInTime;
-	}
-
-	public void setJustInTime(int justInTime) {
-		this.justInTime = justInTime;
-	}
-
-	public String getScheduler() {
-		return scheduler;
-	}
-
-	public void setScheduler(String scheduler) {
-		this.scheduler = scheduler;
-	}
-
-	public String getProviderUrl() {
-		return providerUrl;
-	}
-
-	public void setProviderUrl(String providerUrl) {
-		this.providerUrl = providerUrl;
-	}
-
-	public String getPrincipal() {
-		return principal;
-	}
-
-	public void setPrincipal(String principal) {
-		this.principal = principal;
-	}
-
-	public String getCredentials() {
-		return credentials;
-	}
-
-	public void setCredentials(String credentials) {
-		this.credentials = credentials;
-	}
-
-	public String getFilters() {
-		return filters;
-	}
-
-	public void setFilters(String filters) {
-		this.filters = filters;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public String getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(String createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public String getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public void setModifiedDate(String modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getInstId() {
-		return instId;
-	}
-
-	public void setInstId(String instId) {
-		this.instId = instId;
-	}
-
-	public String getInstName() {
-		return instName;
-	}
-
-	public void setInstName(String instName) {
-		this.instName = instName;
-	}
-
-	public Connectors(String id) {
-		this.id = id;
-	}
-
-	@Override
-	public String toString() {
-		return new JsonPretty().format(this);
-	}
+    @Override
+    public String toString() {
+        return new JsonPretty().format(this);
+    }
 
 }
