1ebe6aff847b3fa7c341c2de00b2d86511c18369
caokhang91 <caokhang91@gmail.com>
2023-05-06 20:12:50 +0700

    test build

--- maxkey-webs/maxkey-gataway/src/main/java/org/gateway/controller/FallbackController.java
+++ maxkey-webs/maxkey-gataway/src/main/java/org/gateway/controller/FallbackController.java
@@ -29,7 +29,7 @@ public class FallbackController {
     public Map<String , Object> defaultFallback() {
         Map<String , Object> map = new HashMap<>();
         map.put("code", 1);
-        map.put("message", "服务异常");
+        map.put("message", "Successful");
         return map;
     }
 }
