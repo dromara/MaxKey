0c02c7f1ecfe21c4b59d2b887a6cafb6fd64cde8
MaxKey <shimingxy@qq.com>
2024-06-27 21:19:44 +0800

    gradle-8.8

--- setEnvVars.bat
+++ setEnvVars.bat
@@ -1,7 +1,7 @@
 echo off
 echo set env
-set JAVA_HOME=D:\IDE\jdk-17.0.2.8
-set GRADLE_HOME=C:\IDE\gradle-8.0.2
+set JAVA_HOME=C:\IDE\jdk-17.0.2.8
+set GRADLE_HOME=C:\IDE\gradle-8.8
 
 call %JAVA_HOME%/bin/java -version
 call %GRADLE_HOME%/bin/gradle -version
