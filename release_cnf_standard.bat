32b0f69c50ec5fce40ca85c806ffa41ea119fbc6
shimingxy <shimingxy@qq.com>
2024-07-19 16:46:19 +0800

    构建方式优化

--- release_cnf_standard.bat
+++ release_cnf_standard.bat
@@ -3,8 +3,6 @@ call setEnvVars.bat
 set START_TIME="%date:~0,10% %time:~0,2%:%time:~3,5%"
 echo start time %START_TIME%
 
-call %GRADLE_HOME%/bin/gradle configStd  -b build_cnf.gradle
+call %GRADLE_HOME%/bin/gradle configStandard -b build_cnf.gradle
 
-
-#pause
 pause
