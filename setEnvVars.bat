6f42b6871c652776eb11d663b68f6c9160a927c0
shimingxy <shimingxy@qq.com>
2025-02-14 11:22:01 +0800

    Update setEnvVars.bat

--- setEnvVars.bat
+++ setEnvVars.bat
@@ -3,7 +3,7 @@ echo set env
 set JAVA_HOME=C:\ide\jdk-17.0.9+9
 set GRADLE_HOME=C:\ide\gradle-8.8
 
-set MXK_VERSION=4.1.5
+set MXK_VERSION=4.1.6
 set MXK_REPOSITORY=maxkeytop
 
 call %JAVA_HOME%/bin/java -version
