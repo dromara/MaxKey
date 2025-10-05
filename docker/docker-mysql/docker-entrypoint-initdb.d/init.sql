1cb5ab7abd943877980cb31120c09cbd4e3ba50e
MaxKey <shimingxy@qq.com>
2025-10-05 09:59:53 +0800

    4.1.9

--- docker/docker-mysql/docker-entrypoint-initdb.d/init.sql
+++ docker/docker-mysql/docker-entrypoint-initdb.d/init.sql
@@ -3,5 +3,4 @@ create database if not exists  `maxkey` /*!40100 DEFAULT CHARACTER SET utf8mb4 C
 use maxkey ;
 
 source /docker-entrypoint-initdb.d/latest/maxkey.sql   ;
-source /docker-entrypoint-initdb.d/latest/maxkey_data.sql   ;
  
