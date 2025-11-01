5b15439393ee4f4b7c45651c8d4408ccceb07b20
MaxKey <shimingxy@qq.com>
2025-11-01 19:16:06 +0800

    appendScriptSrc

--- maxkey-web-frontend/maxkey-web-app/src/assets/transform.js
+++ maxkey-web-frontend/maxkey-web-app/src/assets/transform.js
@@ -1,13 +1,28 @@
 /*
  * Copyright [2022] [MaxKey of copyright http://www.maxkey.top]
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * /passport/login--->/#/passport/login
- * /passport/callback/---->/#/passport/callback/
+ *
+ */
+
+/**
+ * 动态加载js脚本文件
+ * @param {*} scriptSrc
+ */
+function appendScriptSrc(scriptSrc){
+  var script = document.createElement('script');
+  script.src = scriptSrc; // 加载JS文件路径
+  document.body.appendChild(script);
+}
+
+/**
+ *   /passport/login--->/#/passport/login
+ *   /passport/callback/---->/#/passport/callback/
+ * @param {*} transPath
  */
 function locationTransform(transPath){
     var topHref = top.location.href;
@@ -20,9 +35,10 @@ function locationTransform(transPath){
     }
 }
 var transPaths = [
-    '/passport/login', 
+    '/passport/login',
     '/passport/callback/'
 ];
-for (i = 0; i < transPaths.length; i++) { 
+for (i = 0; i < transPaths.length; i++) {
     locationTransform(transPaths[i]);
 }
+
